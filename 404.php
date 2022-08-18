e97c17df8985eebbf4a3c6e442c19d9e19ec3f1d
Annie Kyles <anniek@federation.net.nz>
2022-08-18 15:18:20 +1200

    Init

---
+++ 404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TSB at Federation</title>
+    <meta name="description" content="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="icon" type="image/png" href="./favicon.png">
+</head>
+<body>
+    <div class="content">
+        <section>
+            <p>Sorry, but the page you were trying to view does not exist. <a href="https://tsb.federation.net.nz">Go back to main page</a>.</p>
+        </section>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ACG at Federation</title>
+    <meta name="description" content="">
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <a class="logo logo-acg" href="https://federation.net.nz" target="_blank">
+                <img src="./images/logos/federation-logo-white.svg" alt="Federation logo" title="Federation">
+            </a>
+            <a class="logo logo-fed" href="https://federation.net.nz" target="_blank">
+                <img src="./images/logos/federation-logo-white.svg" alt="Federation logo" title="Federation">
+            </a>
+            <div id="arrow-down"></div>
+        </header>
+        
+        <div class="page">
+            <section class="fade-in">
+                <div class="content">
+                    <h1>Kia ora</h1>
+                    <p class="intro">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ipsa debitis magnam error reprehenderit voluptate perferendis et omnis. Perspiciatis quam ipsam autem consectetur quas amet cum a labore voluptas sequi.</p>
+                </div>
+            </section>
+            <section class="fade-in">
+                <div class="content">
+                    <h2>Manifesto</h2>
+                    <div class="video-container">
+                        <iframe id="video-menifesto" src="https://www.youtube.com/embed/pxfdn7UVUlY?playsinline=0&rel=0" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </section>
+            <section class="fade-in">
+                <div class="content">
+                    <h2>Title</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione cum tempora odio, debitis voluptate modi et, facere ea, nihil quidem fugit! Deserunt rem sunt libero illum numquam dolorum cumque dicta.</p>                
+                    <a class="button" href="./pdfs/test.pdf" target="_blank" title="">Download strategy
+                        
+                        <svg version="1.1" id="Isolation_Mode" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                y="0px" viewBox="0 0 369.6 532" style="enable-background:new 0 0 369.6 532;" xml:space="preserve">
+                            <path d="M113.68,346.64v-43.12H43.12V43.12h283.36v260.4h-70.56v43.12H369.6V0H0v346.64H113.68z"/>
+                            <path d="M206.64,448.56V226.24h-43.68v223.44l-53.76-54.32l-30.8,30.8L184.24,532l105.84-105.84l-30.24-30.8L206.64,448.56z"/>
+                        </svg></a>
+                </div>
+            </section>
+            <section class="fade-in">
+                <div class="content">
+                    <h2>Test</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea aliquam omnis architecto ducimus nostrum quam laboriosam, ratione iure illo dicta recusandae aliquid non vel pariatur? Autem optio dolores nobis voluptas.</p>                
+                    <a class="button" href="./pdfs/test.pdf" target="_blank" title="">Download creative
+                        <svg version="1.1" id="Isolation_Mode" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                y="0px" viewBox="0 0 369.6 532" style="enable-background:new 0 0 369.6 532;" xml:space="preserve">
+                            <path d="M113.68,346.64v-43.12H43.12V43.12h283.36v260.4h-70.56v43.12H369.6V0H0v346.64H113.68z"/>
+                            <path d="M206.64,448.56V226.24h-43.68v223.44l-53.76-54.32l-30.8,30.8L184.24,532l105.84-105.84l-30.24-30.8L206.64,448.56z"/>
+                        </svg>
+                    </a>
+                </div>
+            </section>
+            <section class="fade-in">
+                <div class="content">
+                    <p class="h1">Ngā mihi nui</p>
+                    <p>On behalf of <a href="https://federation.net.nz/about" target="_blank">everyone at Federation</a>, thank you.</p>
+                    <p>We look forward to talking more.</p>
+                </div>
+            </section>
+        </div>
+    </div>
+    <script src="./js/custom.js"></script>
+</body>
+</html>
