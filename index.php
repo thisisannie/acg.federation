7bb1d835bdee83e6e81b0ceb74b95d586c47ed7a
Annie Kyles <anniek@federation.net.nz>
2022-08-18 16:35:03 +1200

    Preliminary content.

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
             <a class="logo logo-acg" href="https://federation.net.nz" target="_blank">
                 <img src="./images/logos/federation-logo-white.svg" alt="Federation logo" title="Federation">
             </a>
-            <a class="logo logo-fed" href="https://federation.net.nz" target="_blank">
-                <img src="./images/logos/federation-logo-white.svg" alt="Federation logo" title="Federation">
+            <a class="logo logo-fed" href="https://www.aucklandcouncil.govt.nz/" target="_blank">
+                <img src="./images/logos/ac-pohutukawa-icon.svg" alt="Federation logo" title="Federation">
             </a>
             <div id="arrow-down"></div>
         </header>
@@ -23,23 +23,17 @@
         <div class="page">
             <section class="fade-in">
                 <div class="content">
-                    <h1>Kia ora</h1>
-                    <p class="intro">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ipsa debitis magnam error reprehenderit voluptate perferendis et omnis. Perspiciatis quam ipsam autem consectetur quas amet cum a labore voluptas sequi.</p>
+                    <h1>Ehara taku toa I te toa takitahi, engari he toa takitini.</h1>
+                    <p class="intro"><em>Success is not the work of an individual, but the work of many.</em><br>
+                    We are Federation. <br>
+                    A federation of minds.</p>
                 </div>
             </section>
             <section class="fade-in">
                 <div class="content">
-                    <h2>Manifesto</h2>
-                    <div class="video-container">
-                        <iframe id="video-menifesto" src="https://www.youtube.com/embed/pxfdn7UVUlY?playsinline=0&rel=0" allowfullscreen></iframe>
-                    </div>
-                </div>
-            </section>
-            <section class="fade-in">
-                <div class="content">
-                    <h2>Title</h2>
+                    <h2>Our people</h2>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione cum tempora odio, debitis voluptate modi et, facere ea, nihil quidem fugit! Deserunt rem sunt libero illum numquam dolorum cumque dicta.</p>                
-                    <a class="button" href="./pdfs/test.pdf" target="_blank" title="">Download strategy
+                    <a class="button" href="./pdfs/test.pdf" target="_blank" title="">Download our people
                         
                         <svg version="1.1" id="Isolation_Mode" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
                                 y="0px" viewBox="0 0 369.6 532" style="enable-background:new 0 0 369.6 532;" xml:space="preserve">
@@ -50,9 +44,22 @@
             </section>
             <section class="fade-in">
                 <div class="content">
-                    <h2>Test</h2>
+                    <h2>Case study</h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea aliquam omnis architecto ducimus nostrum quam laboriosam, ratione iure illo dicta recusandae aliquid non vel pariatur? Autem optio dolores nobis voluptas.</p>                
-                    <a class="button" href="./pdfs/test.pdf" target="_blank" title="">Download creative
+                    <a class="button" href="./pdfs/test.pdf" target="_blank" title="">Download case study
+                        <svg version="1.1" id="Isolation_Mode" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                y="0px" viewBox="0 0 369.6 532" style="enable-background:new 0 0 369.6 532;" xml:space="preserve">
+                            <path d="M113.68,346.64v-43.12H43.12V43.12h283.36v260.4h-70.56v43.12H369.6V0H0v346.64H113.68z"/>
+                            <path d="M206.64,448.56V226.24h-43.68v223.44l-53.76-54.32l-30.8,30.8L184.24,532l105.84-105.84l-30.24-30.8L206.64,448.56z"/>
+                        </svg>
+                    </a>
+                </div>
+            </section>
+            <section class="fade-in">
+                <div class="content">
+                    <h2>Our work</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea aliquam omnis architecto ducimus nostrum quam laboriosam, ratione iure illo dicta recusandae aliquid non vel pariatur? Autem optio dolores nobis voluptas.</p>                
+                    <a class="button" href="./pdfs/test.pdf" target="_blank" title="">Download other work
                         <svg version="1.1" id="Isolation_Mode" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
                                 y="0px" viewBox="0 0 369.6 532" style="enable-background:new 0 0 369.6 532;" xml:space="preserve">
                             <path d="M113.68,346.64v-43.12H43.12V43.12h283.36v260.4h-70.56v43.12H369.6V0H0v346.64H113.68z"/>
@@ -64,8 +71,6 @@
             <section class="fade-in">
                 <div class="content">
                     <p class="h1">Ngā mihi nui</p>
-                    <p>On behalf of <a href="https://federation.net.nz/about" target="_blank">everyone at Federation</a>, thank you.</p>
-                    <p>We look forward to talking more.</p>
                 </div>
             </section>
         </div>
